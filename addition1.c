0abb2be15a7acea9efeb7bf9d84c38311a2221fc
Vivek Admuthe <vivekadmuthe@gmail.com>
2021-03-10 13:46:37 +0530

    Added addition1.c

---
+++ addition1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int num1;
+   int num2;
+   int answer;
+   
+    printf("enter the number: ");
+    scanf("%d",&num1);
+
+   printf("\n enter the number :");
+   scanf("%d",&num2);
+
+
+   answer=num1+num2;
+   printf("\n answer=%d",answer);
+
+   return 0;
+
+
+
+
+}
