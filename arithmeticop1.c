ae9eb049b95ee15e1950b3b52964c046861cbe48
Vivek Admuthe <vivekadmuthe@gmail.com>
2021-03-10 13:51:23 +0530

    added arithmeticop1.c

---
+++ arithmeticop1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+
+   int a=10;
+   int b=13;
+   int c=a+b;
+   printf("%d+%d=%d",a,b,c);
+
+   c=a*b;
+   printf("\n%d*%d=%d",a,b,c);
+
+   c=a-b;
+   printf("\n%d-%d=%d",a,b,c);
+
+   c=a/b;
+   printf("\n%d/%d=%d",a,b,c);
+   return 0;
+
+}
