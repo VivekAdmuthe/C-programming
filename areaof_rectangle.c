d4c5aa56341b76b38951ab4d37bf42fc5a2f6513
Vivek Admuthe <vivekadmuthe@gmail.com>
2021-03-12 16:42:23 +0530

    areaof_rectangle.c

---
+++ areaof_rectangle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int length;
+    int breadth;
+  printf("enter the value of lenght:");
+  scanf("%d",&length);
+  printf("enter the value of breadth:");
+  scanf("%d",&breadth);
+  printf("the area of rectangle is %d cmsq",length*breadth);
+
+
+   return 0;
+
+
+}
