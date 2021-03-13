902beeff48336b708f4b6ca6902f6e9cfb894533
Vivek Admuthe <vivekadmuthe@gmail.com>
2021-03-13 18:59:41 +0530

    NegativeOrPositive.c

---
+++ NegativeOrPositive.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+int main()
+{
+   double num;
+   printf("enter  the num :");
+   scanf("%if",&num);
+
+   if (num<=00.00)
+   {
+       if (num=00.00)
+       {
+           /* code */printf("you enterd zero");
+       }
+       else
+       {
+       printf("the number is negative");
+       }
+       /* code */
+   }
+   else
+   {
+       printf("the number is positive");
+   } 
+   return 0;
+}
