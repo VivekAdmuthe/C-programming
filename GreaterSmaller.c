1bab601ab5a3f4141de7ef354e301bfdd43b1fe7
Vivek Admuthe <vivekadmuthe@gmail.com>
2021-03-13 18:59:06 +0530

    GreaterOrSmaller.c

---
+++ GreaterSmaller.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int a,b;
+   a=12;
+   b=16;
+   if (a>b)
+   {
+        printf("a is greater ");
+   }
+   else
+   {
+       printf("b is greater");
+   }
+return 0;
+}
