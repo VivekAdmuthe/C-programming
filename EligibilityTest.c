8c655c8d998cd486588474c60c9a7033eefc5a10
Vivek Admuthe <vivekadmuthe@gmail.com>
2021-03-13 16:57:32 +0530

    EligibilityTest.c

---
+++ EligibilityTest.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int age;
+ printf("enter your age: ");
+ scanf("%d",&age);
+ if(age>18)
+ {
+     printf("you are eligible");
+
+ }
+ else 
+ {
+     printf("you are not eligible");
+
+ }
+
+
+return 0;
+
+}
