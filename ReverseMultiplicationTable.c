b942375ed14e8e6427c6fe29b8e58f555bf53f25
Vivek Admuthe <vivekadmuthe@gmail.com>
2021-03-15 11:42:53 +0530

    ReverseMultiplicationTable.c

---
+++ ReverseMultiplicationTable.c
@@ -0,0 +1,15 @@
+//Multiplication Table Order //
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i;
+int n;
+printf("Enter the value :");
+scanf("%d",&n);
+printf("The Multiplication Table Of %d is \n",n);
+for(i=10;i>=1;i--){
+    printf("%d*%d=%d\n",n,i,n*i);
+}
+return 0;
+}
