ecb4fa7c5341233679b372cc0e235cb3c47fe778
Vivek Admuthe <vivekadmuthe@gmail.com>
2021-03-17 23:12:50 +0530

    SimplewProgram

---
+++ Multiple-operations.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int x=2,y=3,z=3,k=1;
+int calculation=3*x/y-z+k;
+printf("the calculation is = %d",calculation);
+// 3*2/3-3+1 //
+//   6/3-3+1 //
+//     2-3+1 //
+//      -1+1 //
+//         0 //
+return 0;
+}
