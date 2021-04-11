afaac91c50e9daa8735644054ed3175f6afc1ba5
Vivek Admuthe <vivekadmuthe@gmail.com>
2021-04-11 23:24:27 +0530

    array

---
+++ OneD_array.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int values[5];
+
+    printf("Enter 5 integers: ");
+
+    for (int i = 0; i < 5; ++i)
+    {
+        scanf("%d", &values[i]);
+    }
+
+    printf("Displaying integers: ");
+    printf("\n");
+    for (int i = 0; i < 5; ++i)
+    {
+        printf("%d\n", values[i]);
+    }
+    return 0;
+}
