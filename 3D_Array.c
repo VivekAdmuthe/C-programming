76489f906766e178bbe7d9492be32a280f349399
Vivek Admuthe <vivekadmuthe@gmail.com>
2021-04-12 22:14:11 +0530

    3D_Array.c

---
+++ 3D_Array.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+int main()
+{
+  int test[2][3][2];
+
+  printf("Enter 12 values: \n");
+
+  for (int i = 0; i < 2; ++i)
+  {
+    for (int j = 0; j < 3; ++j)
+    {
+      for (int k = 0; k < 2; ++k)
+      {
+        scanf("%d", &test[i][j][k]);
+      }
+    }
+  }
+
+  // Printing values with proper index.
+
+  printf("\nDisplaying values:\n");
+  for (int i = 0; i < 2; ++i)
+  {
+    for (int j = 0; j < 3; ++j)
+    {
+      for (int k = 0; k < 2; ++k)
+      {
+        printf("test[%d][%d][%d] = %d\n", i, j, k, test[i][j][k]);
+      }
+    }
+  }
+
+  return 0;
+}
